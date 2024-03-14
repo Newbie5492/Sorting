101d8f4aa829c7d98a7da5aadf33b330bcdd8426
Anubhav Pathania <115573505+Newbie5492@users.noreply.github.com>
2024-03-14 23:16:37 +0530

    Create Selectionsort.js

---
+++ Selectionsort.js
@@ -0,0 +1,19 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+
+  return arr;
+}
+
+const arr = [1, 2, 1, 9, 3, 7, 4];
+console.log(selectionSort(arr).join(" "));
